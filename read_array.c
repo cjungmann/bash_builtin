76afe073097929436bd97215f5780ad6d07c766c
Chuck Jungmann <chuck@cpjj.net>
2023-04-11 17:11:45 -0500

    commit recent stuff.

---
+++ read_array.c
@@ -0,0 +1,76 @@
+#include <builtins.h>
+#include <shell.h>
+#include <builtins/bashgetopt.h>  // for internal_getopt(), etc.
+#include <builtins/common.h>      // for no_options()
+
+#include <stdio.h>
+
+
+void dump_array(const char *name)
+{
+   SHELL_VAR *svar;
+   svar = find_variable(name);
+   if (svar)
+   {
+      ARRAY *loc_array;
+      loc_array = array_cell(svar);
+
+      int el_count = 0;
+      el_count = array_num_elements(loc_array);
+      printf("The array, %s, contains %d elements.\n", name, el_count);
+
+      ARRAY_ELEMENT *el;
+      el = element_forw(loc_array->head);
+
+      // Note the following end test: test for NULL will not work
+      while(el != loc_array->head)
+      {
+         printf("   %s\n", element_value(el));
+         el = element_forw(el);
+      }
+   }
+   else
+      printf("Failed to find variable '%s'\n", name);
+}
+
+
+static int read_array(WORD_LIST *list)
+{
+   // There should be no options, recognized as arguments
+   // that begin with a hyphen:
+   if (no_options(list))
+      return EX_USAGE;
+
+   list = loptend;
+
+   if (list == 0)
+   {
+      builtin_usage();
+      return EX_USAGE;
+   }
+
+   WORD_LIST *lptr = list;
+   while (lptr != NULL)
+   {
+      dump_array((lptr->word->word));
+      lptr = lptr->next;
+   }
+
+   return EXECUTION_SUCCESS;
+}
+
+static char *desc_read_array[] = {
+   "This function accepts a variable name of an array.",
+   "Succeess in this code is accomplished if the name",
+   "can be attached and the elements can be printed."
+};
+
+
+struct builtin read_array_struct = {
+   .name      = "read_array",
+   .function  = read_array,
+   .flags     = BUILTIN_ENABLED,
+   .long_doc  = desc_read_array,
+   .short_doc = "read_array array_name",
+   .handle    = 0
+};
---
+++ write_array.c
@@ -0,0 +1,70 @@
+#include <builtins.h>
+#include <shell.h>
+#include <builtins/bashgetopt.h>  // for internal_getopt(), etc.
+#include <builtins/common.h>      // for no_options()
+
+#include <stdio.h>
+
+
+void replace_array(const char *name)
+{
+   SHELL_VAR *svar;
+   svar = find_variable(name);
+   if (svar)
+   {
+      ARRAY *array = array_cell(svar);
+      array_flush(array);
+
+      array_insert(array, 0, "un");
+      array_insert(array, 1, "deux");
+      array_insert(array, 2, "trois");
+      array_insert(array, 3, "quatre");
+      array_insert(array, 4, "cinq");
+      array_insert(array, 5, "six");
+   }
+}
+
+static int write_array(WORD_LIST *list)
+{
+   // There should be no options, recognized as arguments
+   // that begin with a hyphen:
+   if (no_options(list))
+      return EX_USAGE;
+
+   list = loptend;
+
+   if (list == 0)
+   {
+      builtin_usage();
+      return EX_USAGE;
+   }
+
+   WORD_LIST *lptr = list;
+   while (lptr != NULL)
+   {
+      const char *word = lptr->word->word;
+      if (legal_identifier(word))
+         replace_array(word);
+      else
+         printf("Name ref '%s' is not a legal identifier.\n", word);
+
+      lptr = lptr->next;
+   }
+
+   return EXECUTION_SUCCESS;
+}
+
+static char *desc_write_array[] = {
+   "This function will accept the name of an array variable,",
+   "which will be cleared out and filled with new content."
+};
+
+
+struct builtin write_array_struct = {
+   .name      = "write_array",
+   .function  = write_array,
+   .flags     = BUILTIN_ENABLED,
+   .long_doc  = desc_write_array,
+   .short_doc = "write_array array_name",
+   .handle    = 0
+};
