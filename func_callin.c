4dc379e4db730111ef030bedf9d5b0bfc9034dcd
Chuck Jungmann <chuck@cpjj.net>
2023-06-06 19:35:20 -0500

    Many changes.

--- func_callin.c
+++ func_callin.c
@@ -5,58 +5,70 @@
 #include <builtins/common.h>      // for no_options()
 
 #include <stdio.h>
+#include <stdarg.h>
 
 #include "word_list_stack.h"
+
+// Mysterious "line" value of COMMAND object: -538976289
+// Is this number random or repeated?
 
 // Copied from execute_cmd.h from bash source code, using code in command.h.
 extern int execute_command PARAMS((COMMAND *));
 extern int execute_shell_function PARAMS((SHELL_VAR *, WORD_LIST *));
 
-// Copied from bash_src/externs.h, function code in parse.y, not found
-extern COMMAND *parse_string_to_command PARAMS((char *, int));
-// Alternative suggested in ChangeLog is xparse_dolparen (dollar parentheses)
-
-// Let's try to enable the second command, bundle it and return it to the caller:
-extern int enable_shell_command(char* name, int disable_p);
-
-
 SHELL_VAR *make_callback_function(void)
 {
-   char command[] = "echo \"hello from make_callback_function.\";";
-   COMMAND *cmd = parse_string_to_command(command, 0);
-   if (cmd)
-      printf("Successfully created a command.\n");
-   else
-      printf("Disgracefully failed to creat a command.\n");
+   // Make WORD_LIST with a command and its arguments
+   WORD_LIST *head = NULL, *tail = NULL;
+   const char *wlist[] = {
+      "callout_target",
+      "imaginary",
+      "pretend",
+      "bogus",
+      // "echo",
+      // "Hello from the make_callback_function",
+      NULL
+   };
 
-   // char *fname = xparse_dolparen("callin", command, 0, 0);
-   // if (fname)
-   //    printf("fname is %s.\n", fname);
-   // else
-   //    printf("fname was not generated for some unknown reason.\n");
+   const char **wlarray = wlist;
+   WL_WALK(head, tail, wlarray);
+
+   // Make a COMMAND object, identify as `cm_simple` (command.h, enum command_type)
+   COMMAND *cmd = (COMMAND*)alloca(sizeof(COMMAND));
+   memset(cmd, 0, sizeof(COMMAND));
+   cmd->type = cm_simple;
+   cmd->value.Simple = (SIMPLE_COM*)alloca(sizeof(SIMPLE_COM));
+   memset(cmd->value.Simple, 0, sizeof(SIMPLE_COM));
+   cmd->value.Simple->words = head;
+
+   execute_command(cmd);
+
 
    return NULL;
 }
 
-int call_with_execute_command(const char *function_name)
+int call_with_execute_command(const char *function_name, ...)
 {
-   SHELL_VAR *sv = make_callback_function();
+   SHELL_VAR *sv = find_function(function_name);
 
-   // Array of strings to transform into WORD_LIST
-   const char *words[]={
-      function_name,
-      "myfunc",
-      NULL
-   };
+   if (sv == NULL)
+   {
+      fprintf(stderr, "FAILED TO FIND function \x1b[32;1m%s\x1b[m.\n", function_name);
+      return EXECUTION_FAILURE;
+   }
 
-   // Prepare arguments to WL_APPEND macro
-   WORD_LIST *tail=NULL, *head=NULL;
-   const char **strarray = words;
+   WORD_LIST *head = NULL, *tail = NULL;
 
-   WL_WALK(head, tail, strarray);
+   WL_APPEND(tail, function_name);
+   head = tail;
 
-   if (sv)
-      WL_APPEND(tail,"newfunc");
+   // Add any extra parameters to the WORD_LIST:
+   va_list args_list;
+   va_start(args_list, function_name);
+   const char *val;
+   while ((val = va_arg(args_list, const char*)))
+      WL_APPEND(tail, val);
+   va_end(args_list);
 
    int flags = CMD_INHIBIT_EXPANSION | CMD_STDPATH;
 
@@ -64,8 +76,7 @@ int call_with_execute_command(const char *function_name)
    command = make_bare_simple_command();
    command->value.Simple->words = head;
    command->value.Simple->redirects = (REDIRECT *)NULL;
-   command->value.Simple->flags = flags;
-   command->flags = flags;
+   command->flags = command->value.Simple->flags = flags;
 
    return execute_command(command);
 }
@@ -80,16 +91,53 @@ static int func_callin(WORD_LIST *list)
       return EX_USAGE;
    }
 
-   enable_shell_command(NULL, 0);
+    /*****************************************
+    * Begin constructing COMMAND for callin
+    *****************************************/
+
+   // Begin constructing stack-based SHELL_VAR* containing
+   // a stack-based COMMAND.
+   // Make WORD_LIST with a command and its arguments
+   const char *bound_function_name = "CALLIN_CALLBACK";
+   WORD_LIST *head = NULL, *tail = NULL;
+   const char *wlist[] = {
+      "callout_target",
+      bound_function_name,
+      "imaginary",
+      "pretend",
+      "bogus",
+      // "echo",
+      // "Hello from the make_callback_function",
+      NULL
+   };
+   const char **wlarray = wlist;
+   WL_WALK(head, tail, wlarray);
+
+   // Make a COMMAND object, identify as `cm_simple` (command.h, enum command_type)
+   COMMAND *cmd = (COMMAND*)alloca(sizeof(COMMAND));
+   memset(cmd, 0, sizeof(COMMAND));
+   cmd->type = cm_simple;
+   cmd->value.Simple = (SIMPLE_COM*)alloca(sizeof(SIMPLE_COM));
+   memset(cmd->value.Simple, 0, sizeof(SIMPLE_COM));
+   cmd->value.Simple->words = head;
+
+   SHELL_VAR* svar_command = bind_function(bound_function_name, cmd);
+    /*****************************************
+    * DONE constructing COMMAND for callin
+    *****************************************/
 
    const char *callback_function = list->word->word;
-   SHELL_VAR *svar = find_function(callback_function);
-   if (svar)
-      call_with_execute_command(callback_function);
+   if (svar_command)
+   {
+      printf("Made a nice SVAR variable of type COMMAND of name %s.\n", bound_function_name);
+      return call_with_execute_command(callback_function, bound_function_name, NULL);
+   }
    else
-      printf("FAILED TO FIND function \x1b[32;1m%s\x1b[m.\n", callback_function);
+   {
+      fprintf(stderr, "Failed to bind_command for function %s.\n", callback_function);
+      return EXECUTION_FAILURE;
+   }
 
-   return EXECUTION_SUCCESS;
 }
 
 
