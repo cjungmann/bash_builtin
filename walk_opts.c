8b63a71c5667d3bbc1d6ccb7c61a1064b07dbff6
Chuck Jungmann <chuck@cpjj.net>
2023-06-13 11:31:35 -0500

    Experimenting with # for option list to accept numbers only.

--- walk_opts.c
+++ walk_opts.c
@@ -6,6 +6,17 @@
 
 #include <stdio.h>
 
+void walk_raw_arguments(WORD_LIST *list)
+{
+   WORD_LIST *ptr = list;
+   int count = 0;
+   while (ptr)
+   {
+      printf("%02d: '%s'\n", ++count, ptr->word->word);
+      ptr = ptr->next;
+   }
+}
+
 static int walk_opts(WORD_LIST *list)
 {
    int opt;
@@ -14,20 +25,37 @@ static int walk_opts(WORD_LIST *list)
    int opt_b=0;
    int opt_c=0;
 
+   const char * __attribute__((unused))dopt = NULL;
+   int __attribute__ ((unused)) eopt = -1;
+
    reset_internal_getopt();
-   while ((opt = internal_getopt(list, "abc")) != -1)
+   while ((opt = internal_getopt(list, "abcd:e#")) != -1)
    {
       switch (opt)
       {
          case 'a':
+            printf("Setting flag 'a' on.\n");
             opt_a = 1;
             break;
          case 'b':
+            printf("Setting flag 'b' on.\n");
             opt_b = 1;
             break;
          case 'c':
+            printf("Setting flag 'c' on.\n");
             opt_c = 1;
             break;
+         case 'd':
+            printf("Setting option 'd' to '%s'\n", list_optarg);
+            dopt = list_optarg;
+            break;
+         case 'e':
+            printf("Setting option 'e' to %d.\n", atoi(list_optarg));
+            eopt = atoi(list_optarg);
+            break;
+         case '?':
+            printf("Bad argument type.\n");
+            goto bad_arg_exit;
          default:
             printf("Unexpected argument %c.\n", opt);
             printf("A normal program would exit with an error here.\n");
@@ -35,10 +63,14 @@ static int walk_opts(WORD_LIST *list)
       }
    }
 
+  bad_arg_exit:
+
    printf("After running the walk_opts program,\n");
    printf("  opt_a = %d\n", opt_a);
    printf("  opt_b = %d\n", opt_b);
    printf("  opt_c = %d\n", opt_c);
+
+   walk_raw_arguments(list);
 
    fflush(stdout);
 
