374fa5b0f5963ac7e111d0a5a9bb45934193e559
Chuck Jungmann <chuck@cpjj.net>
2024-04-02 12:00:57 -0500

    Using new format for managing tables/actions/help, etc.

--- Templates/basic_handle/action_declare.c
+++ Templates/basic_handle/action_declare.c
@@ -1,64 +1,69 @@
-#include "dispatcher.h"
-#include "argeater_setters.h"
-#include "error_handling.h"
-#include <stdio.h>
+/* -*- mode:c -*- */
+/**
+ * @file action_declare.def
+ * @brief Declare provisions for implementing a help display function.
+ */
+
+#include "handle.h"            // for H_TEMPLATE (handle) prototype
+#include "dispatcher.h"        // for AVERB definition
+#include "argeater_setters.h"  // argeater_setters sending errors to TEMPLATE_ERROR
+#include "error_handling.h"    // for ERROR_SINK function pointer
+#include <stdio.h>             // printf
+#include <stdbool.h>           // for bool data type
 
 /***** CONSTRUCT THE ARGUMENT MAP *****/
 
-static bool help_flag;
+// File-scope status variable(s) that will contain the
+// command-line argument settings after the argument map
+// to which they are attached is used by argeater's
+// `argeater_process` function.
 static FILE *file;
 static const char *handle_name;
 
+// Array of items that associate status variables to the means
+// to access them and text to be used in a help display:
 AE_ITEM declare_actions[] = {
-   {(const char **)&help_flag, "help", 'h', AET_FLAG_OPTION,
-    "Show TEMPLATE declare usage", NULL, TEMPLATE_argeater_bool_setter },
-
-   {&handle_name, "name", '\0',  AET_ARGUMENT,
-    "Name of new handle", NULL, argeater_string_setter},
-
    {(const char **)&file, "file", '\0', AET_ARGUMENT,
-    "File stream to read", NULL, TEMPLATE_argeater_stream_setter }
+    "File stream to read", NULL, TEMPLATE_argeater_stream_setter },
+
+   {&handle_name, "handle_name", '\0', AET_ARGUMENT,
+    "Name of new handle", NULL, argeater_string_setter }
 };
 
+// File-scope instantiation of argument map to be used
+// by the `argeater_process` function.
 AE_MAP TEMPLATE_declare_arg_map = INIT_MAP(declare_actions);
 
+/***** END CONSTRUCTION OF THE ARGUMENT MAP *****/
 
-/*****  *****/
+/***** Action Implementation *****/
 
 int TEMPLATE_declare(H_TEMPLATE *handle, ACLONE *args)
 {
    int exit_code = EXECUTION_FAILURE;
 
-   help_flag = false;
    file = NULL;
    handle_name = NULL;
 
    if (argeater_process(args, &TEMPLATE_declare_arg_map))
    {
-      if (handle_name == NULL)
-         (*ERROR_SINK)("TEMPLATE:declare requires a handle name");
-      else if (file==NULL)
-         (*ERROR_SINK)("TEMPLATE:declare requires a file name");
+      // Handle allocated resources (open stream) first
+      // to ensure they are deallocated upon exit:
+      if (file==NULL)
+         (*ERROR_SINK)("TEMPLATE:declare failed to open required file");
       else
       {
-         SHELL_VAR *var = find_variable(handle_name);
-         if (var)
+         if (handle_name)
          {
-            if (TEMPLATE_p(var))
-               (*ERROR_SINK)("TEMPLATE:declare variable '%s' "
-                             "is already a TEMPLATE handle",
-                             handle_name);
-            else
-            {
-               (*ERROR_SINK)("TEMPLATE:declare variable '%s' "
-                             "already exists");
-            }
-         }
-         else
-         {
-            printf("We would be creating the new handle here.");
+            printf("Here we go, the imaginary handle is ready.\n");
             exit_code = EXECUTION_SUCCESS;
          }
+         else
+            (*ERROR_SINK)("TEMPLATE:declare requires new handle name");
+
+         // Close the file after using it:
+         if (file != stdin)
+            fclose(file);
       }
    }
    else
@@ -66,3 +71,14 @@ int TEMPLATE_declare(H_TEMPLATE *handle, ACLONE *args)
 
    return exit_code;
 }
+
+/***** AVERB for action table *****/
+
+AVERB TEMPLATE_declare_verb = {
+   "declare",                            // verb name to match
+   "Create a new TEMPLATE handle",       // description to use for 'help' action
+   "TEMPLATE declare [new_handle_name]", // invocation of verb
+   false,                                // true if handle must already be declared
+   TEMPLATE_declare,                     // reference to action function
+   &TEMPLATE_declare_arg_map             // reference to argument map (for help display)
+};
--- Templates/basic_handle/action_help.c
+++ Templates/basic_handle/action_help.c
@@ -1,3 +1,8 @@
+/**
+ * @file action_help.c
+ * @brief Boilerplate provisions for implementing a help display function.
+ */
+
 #include "dispatcher.h"
 #include "argeater_setters.h"
 #include "error_handling.h"
@@ -6,40 +11,55 @@
 
 // The following includes provide access to maps for help information
 #include "table_of_actions.h"
-#include "dispatcher.h"
 
-
-void show_verb_help(AE_MAP *map, const char *verb)
+/**
+ * @brief Generic help display function
+ * @param "verb"   action table entry for which help is being displayed.
+ */
+void show_verb_help(const AVERB *verb)
 {
-   const char *tname = "TEMPLATE:";
+   const char *tname = "TEMPLATE ";
    int pre_len = strlen(tname);
-   int verb_len = strlen(verb);
+   int verb_len = strlen(verb->name);
    char *buff = alloca(pre_len + verb_len + 1);
    char *ptr = buff;
    memcpy(ptr, tname, pre_len);
    ptr += pre_len;
-   memcpy(ptr, verb, verb_len);
+   memcpy(ptr, verb->name, verb_len);
    ptr += verb_len;
    *ptr = '\0';
 
-   argeater_show_usage(map, buff);
+   argeater_show_usage(verb->arg_map, buff);
+
+   printf("\n%s\n", verb->desc);
 
    // Indent info 3 spaces:
-   argeater_show_options(map, 3);
-   argeater_show_arguments(map, 3);
+   argeater_show_arguments(verb->arg_map, 3);
+   argeater_show_options(verb->arg_map, 3);
 }
 
 /***** CONSTRUCT THE ARGUMENT MAP *****/
 
+// File-scope status variable(s) that will contain the
+// command-line argument settings after the argument map
+// to which they are attached is used by argeater's
+// `argeater_process` function.
 static const char *verb_req = NULL;
 
+// Array of items that associate status variables to the means
+// to access them and text to be used in a help display:
 AE_ITEM help_actions[] = {
-   { &verb_req, "verb", '\0', AET_ARGUMENT,
-     "Display help", NULL, argeater_string_setter}
+   { &verb_req, "action", '\0', AET_ARGUMENT,
+     "Display help about [action]", NULL, argeater_string_setter}
 };
 
+// File-scope instantiation of argument map to be used
+// by the `argeater_process` function.
 AE_MAP TEMPLATE_help_arg_map = INIT_MAP(help_actions);
 
+/***** END CONSTRUCTION OF THE ARGUMENT MAP *****/
+
+/***** Action Implementation *****/
 
 int TEMPLATE_help(H_TEMPLATE* handle, ACLONE *args)
 {
@@ -56,7 +76,7 @@ int TEMPLATE_help(H_TEMPLATE* handle, ACLONE *args)
                                  verb_req);
          if (verb)
          {
-            show_verb_help(verb->arg_map, verb->name);
+            show_verb_help(verb);
             exit_code = EXECUTION_SUCCESS;
          }
          else
@@ -64,11 +84,15 @@ int TEMPLATE_help(H_TEMPLATE* handle, ACLONE *args)
       }
       else
       {
-         AVERB *verb = TEMPLATE_action_table;
-         AVERB *end = verb + TEMPLATE_actions_count;
+         printf("Access help on any of the following actions by entering:\n"
+                "   TEMPLATE help [action]\n\n"
+                "List of available actions:\n");
+
+         AVERB **verb = TEMPLATE_action_table;
+         AVERB **end = verb + TEMPLATE_actions_count;
          while (verb < end)
          {
-            printf("TEMPLATE action '%s'\n", verb->name);
+            printf("   %s\n", (*verb)->name);
             ++verb;
          }
          exit_code = EXECUTION_SUCCESS;
@@ -78,3 +102,14 @@ int TEMPLATE_help(H_TEMPLATE* handle, ACLONE *args)
 
    return exit_code;
 }
+
+/***** AVERB for action table *****/
+
+AVERB TEMPLATE_help_verb = {
+   "help",                              // verb name to match
+   "Show help display for an action",   // description to use for 'help' action
+   "TEMPLATE help [verb]",              // invocation of verb
+   false,                               // handle not required to show help
+   TEMPLATE_help,                       // reference to action function
+   &TEMPLATE_help_arg_map               // reference to argument map (for help display)
+};
