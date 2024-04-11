22157982674c8a43d51061a7a5224acb6f40fcdf
Chuck Jungmann <chuck@cpjj.net>
2024-04-11 10:02:25 -0500

    Wrap handle id/extraction function/macros in Doxygen group.

--- Templates/basic_handle/handle.h
+++ Templates/basic_handle/handle.h
@@ -11,7 +11,17 @@ typedef struct TEMPLATE_handle {
    void *data;
 } H_TEMPLATE;
 
+/**
+ * @defgroup Bash-modelled interface between SHELL_VAR* and TEMPLATE_handle*
+ *
+ * To follow Bash code convention and to promote understanding thereof,
+ * these functions use established suffixes for identifying and accessing
+ * the content of a Bash SHELL_VAR.
+ * @{
+ */
 bool TEMPLATE_p(SHELL_VAR *var);
 #define TEMPLATE_cell(var) ((H_TEMPLATE*)(var)->value)
+/** @} */
+
 
 #endif
