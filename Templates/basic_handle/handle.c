77179fbf8640efe3c20bbf3440ed5577a8f3b6e1
Chuck Jungmann <chuck@cpjj.net>
2024-04-10 21:40:03 -0500

    Make default test recognize the default (unusable/recognizable/unoperational) handle.

--- Templates/basic_handle/handle.c
+++ Templates/basic_handle/handle.c
@@ -3,6 +3,7 @@
 bool TEMPLATE_p(SHELL_VAR *var)
 {
    if (var && var->attributes & att_special)
-      return 0 == strcmp(TEMPLATE_cell(var)->id, TEMPLATE_HANDLE_ID);
+      return (0 == strcmp((char*)var->value, TEMPLATE_HANDLE_ID));
+
    return false;
 }
