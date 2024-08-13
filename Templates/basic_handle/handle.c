dcfb306c6e9725fb8bd9eccf04a18c807e8b36ef
Chuck Jungmann <chuck@cpjj.net>
2024-08-13 08:08:18 -0500

    Using new generic string-packing functions to save argument values in handle.

--- Templates/basic_handle/handle.c
+++ Templates/basic_handle/handle.c
@@ -1,5 +1,8 @@
 #include "handle.h"
+#include "TEMPLATE_utilities.h"
 #include "error_handling.h"
+
+#include <assert.h>
 
 bool TEMPLATE_p(SHELL_VAR *var)
 {
@@ -7,4 +10,42 @@ bool TEMPLATE_p(SHELL_VAR *var)
       return (0 == strcmp((char*)var->value, TEMPLATE_HANDLE_ID));
 
    return false;
+}
+
+int TEMPLATE_calc_handle_size(const char *name, const char *value, const char *callback)
+{
+   int totsize = sizeof(TEMPLATEH);
+   totsize += get_string_saved_len(name);
+   totsize += get_string_saved_len(value);
+   totsize += get_string_saved_len(callback);
+
+   return totsize;
+}
+
+TEMPLATEH* TEMPLATE_initialize_handle(char *buffer,
+                                      int buffer_len,
+                                      const char *name,
+                                      const char *value,
+                                      const char *callback)
+{
+   memset(buffer, 0, buffer_len);
+   TEMPLATEH *handle = (TEMPLATEH*)buffer;
+   memcpy(buffer, TEMPLATE_HANDLE_ID, sizeof(TEMPLATE_HANDLE_ID));
+
+   char *cur_buff = buffer;
+   char *end_buff = buffer + buffer_len;
+
+   // skip past the handle struct
+   cur_buff += sizeof(TEMPLATEH);
+
+   pack_string_in_block(&handle->data.name, &cur_buff, end_buff, name);
+   pack_string_in_block(&handle->data.value, &cur_buff, end_buff, value);
+   pack_string_in_block(&handle->data.callback, &cur_buff, end_buff, callback);
+
+   assert(cur_buff == end_buff);
+
+   // If necessary, set other handle data, including settings
+   // contingent on the content of any submitted strings
+
+   return handle;
 }
--- Templates/basic_handle/handle.h
+++ Templates/basic_handle/handle.h
@@ -6,10 +6,43 @@
 
 #define TEMPLATE_HANDLE_ID "TEMPLATE handle"
 
+/**
+ * @brief Example of struct to 'declare' argument strings
+ *
+ * Add and remove struct members according to your needs
+ */
+typedef struct TEMPLATE_data {
+   const char *name;
+   const char *value;
+   const char *callback;
+} D_TEMPLATE;
+
 typedef struct TEMPLATE_handle {
    char id[sizeof(TEMPLATE_HANDLE_ID)+1];
-   void *data;
-} H_TEMPLATE;
+   D_TEMPLATE data;
+} TEMPLATEH;
+
+/**
+ * @defgroup IMHEADER
+ * @brief Predict size and populate memory for immutable handle
+ *
+ * In general, the arguments should match the strings used in the
+ * TEMPLATE_data struct.  The actual strings will be saved after the
+ * final data member of TEMPLATE_data, with the addresses of the
+ * strings saved in the TEMPLATE_data data.
+ * @{
+ */
+int TEMPLATE_calc_handle_size(const char *name,
+                              const char *value,
+                              const char *callback);
+
+TEMPLATEH * TEMPLATE_initialize_handle(char *buffer,
+                                       int buffer_len,
+                                       const char *name,
+                                       const char *value,
+                                       const char *callback);
+
+/** @} */
 
 /**
  * @defgroup Bash-modelled interface between SHELL_VAR* and TEMPLATE_handle*
@@ -20,7 +53,7 @@ typedef struct TEMPLATE_handle {
  * @{
  */
 bool TEMPLATE_p(SHELL_VAR *var);
-#define TEMPLATE_cell(var) ((H_TEMPLATE*)(var)->value)
+#define TEMPLATE_cell(var) ((TEMPLATEH*)(var)->value)
 /** @} */
 
 
