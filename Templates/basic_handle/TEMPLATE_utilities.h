d569a5ee5ca55ca125384f053794da600953830b
Chuck Jungmann <chuck@cpjj.net>
2026-02-18 15:33:21 -0600

    Added more flexible saveprintf and savevprint for printing strings to a buffer.

--- Templates/basic_handle/TEMPLATE_utilities.h
+++ Templates/basic_handle/TEMPLATE_utilities.h
@@ -2,12 +2,24 @@
 #define TEMPLATE_UTILITIES_H
 
 #include <stdbool.h>
+#include <stdarg.h>
 
+/**
+ * @defgroup string_block Save consecutive strings into a single memory block
+ * These functions are to be used to pack a set of strings
+ * after the fixed-length part of a handle.
+ * @{
+ */
 int get_string_saved_len(const char *str);
 
 bool pack_string_in_block(const char **string,
                           char **buff_pointer,
                           char *buff_limit,
                           const char *value);
+
+/** @} */
+
+char* savevprintf(const char *format, va_list ap_arg);
+char* saveprintf(const char *format, ...);
 
 #endif
